54858f201e33a411655dc59e62abb44377d7f4c4
songyu8285 <songyu8285@gmail.com>
2014-12-30 15:26:51 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var queryString = require("querystring");
 
-server.listen(server_port, server_ip_address, function(){
-  console.log("Listening on " + server_ip_address + ", server_port " + port)
-});
+
+
+http.createServer(function (req, res) {
+    var fileStr = fs.readFileSync('./index.html', 'utf8');
+    res.end(fileStr);
+}).listen(80);
+console.log('listenning port 80');
